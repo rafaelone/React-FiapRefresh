cda9e271014e3b6c3fcf0515382245b256350318
FIAP <FIAP>
2019-10-26 16:10:41 -0300

    mudado layout

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import './index.css'
 import DrawerToggle from '../drawerToggle';
+import Logo from '../../assets/logo.png'
 
 const header =  (props)  =>  (
     <header>
         <nav className="navbar">
             <DrawerToggle />
+           <div className="navbar-logo">
             <span>Mercado livre</span>
+            <img src={Logo} alt="Logo mercado livre"/>
+           </div>
             <ul className="navbar-list">
                 <li className="navbar-item">
                     <NavLink to={'/'}>Home</NavLink>
